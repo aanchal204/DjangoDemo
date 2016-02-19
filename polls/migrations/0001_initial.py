7724c87a205d9711a8148a0d0433db4eb5ff3508
Aanchal Dalmia <aanchal204@yahoo.in>
2016-02-20 00:41:30 +0530

    Added models

---
+++ polls/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Choice',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('choice', models.CharField(max_length=200)),
+                ('votes', models.IntegerField(default=0)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('question', models.CharField(max_length=500)),
+                ('publication_date', models.DateTimeField(verbose_name='date published')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='choice',
+            name='question',
+            field=models.ForeignKey(to='polls.Question'),
+        ),
+    ]
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=500)
+    publication_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question
+
+    def was_published_recently(self):
+        return self.publication_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice = models.CharField(max_length = 200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice
